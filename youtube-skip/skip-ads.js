e93eb038a2fe00f5336b1cff63fb6e5e96da3fc7
Milind Brahme <milindbrahme@Milinds-MacBook-Air.local>
2022-01-28 19:46:03 -0500

    28jan2022

---
+++ youtube-skip/skip-ads.js
@@ -0,0 +1,6 @@
+
+id = setInterval(() => { document.querySelector("[class^=ytp-ad-skip-button]").click() }, 3000)
+
+//<div class="ytp-ad-text ytp-ad-skip-button-text" id="ad-text:6v" style="">Skip Ad</div>
+
+//document.querySelector("#ad-text\\:6v")
